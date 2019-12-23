d590b646b17a84fee150decd3f3069bc996de4d4
Egor Belibov <egor@belibov.dev>
2019-12-23 11:57:21 +0000

    Fix project indentation (True Tabs)

--- clock_of_clocks/lib/styles/gradients.dart
+++ clock_of_clocks/lib/styles/gradients.dart
@@ -3,13 +3,13 @@ import 'package:flutter/widgets.dart';
 import 'colors.dart';
 
 Gradient primaryGradient(BuildContext context) {
-  return RadialGradient(
-    center: const Alignment(0.0, 0.1),
-    radius: 1,
-    colors: [
-      themeBasedColor(context, PaletteColor.primaryGradientColor),
-      themeBasedColor(context, PaletteColor.secondaryGradientColor),
-    ],
-    stops: [0.4, 1.0],
-  );
+	return RadialGradient(
+		center: const Alignment(0.0, 0.1),
+		radius: 1,
+		colors: [
+			themeBasedColor(context, PaletteColor.primaryGradientColor),
+			themeBasedColor(context, PaletteColor.secondaryGradientColor),
+		],
+		stops: [0.4, 1.0],
+	);
 }
