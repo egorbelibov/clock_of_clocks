326e94873b55e573b798b4f3faea111830570eda
Egor Belibov <egor@belibov.dev>
2019-12-23 12:17:27 +0000

    Fix indentation (tabs -> spaces) -> Editor issues

--- clock_of_clocks/lib/components/clock_hand.dart
+++ clock_of_clocks/lib/components/clock_hand.dart
@@ -3,33 +3,33 @@ import 'package:flutter/widgets.dart';
 import '../styles/colors.dart';
 
 class ClockHand extends StatelessWidget {
-	final int id;
-	final Color color;
-	final double angle;
+  final int id;
+  final Color color;
+  final double angle;
 
-	ClockHand({
-		@required this.id,
-		@required this.angle,
-		this.color,
-	}) : assert(angle != null);
+  ClockHand({
+    @required this.id,
+    @required this.angle,
+    this.color,
+  }) : assert(angle != null);
 
-	@override
-	Widget build(BuildContext context) {
-		return FractionallySizedBox(
-			// TODO: Experiment with other widthFactor vls - like 0.5 and 0.6
-			widthFactor: 0.55,
-			heightFactor: 0.12,
-			child: AnimatedContainer(
-				duration: Duration(milliseconds: 1000),
-				decoration: BoxDecoration(
-					borderRadius: BorderRadius.all(Radius.circular(5)),
-					color: color ??
-							themeBasedColor(
-								context,
-								PaletteColor.primaryColor,
-							), // Experiment with random color values (I think it looks cool)
-				),
-			),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      // TODO: Experiment with other widthFactor vls - like 0.5 and 0.6
+      widthFactor: 0.55,
+      heightFactor: 0.12,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 1000),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: color ??
+              themeBasedColor(
+                context,
+                PaletteColor.primaryColor,
+              ), // Experiment with random color values (I think it looks cool)
+        ),
+      ),
+    );
+  }
 }
