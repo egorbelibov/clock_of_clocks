4a3ed08a6efb6dde78a05340c1c39c2894ebfc16
Egor Belibov <egor@belibov.dev>
2019-12-28 17:38:25 +0000

    Add [clock_digit_arranger.dart]
    Responsible for arranging together a List<AnalogClockModel>'s
    based on their [digitIndex] and defualt [digit] arrangement.
    
    It uses the default [clockDigitArrangement] to get the default
    [digit] arrangement. Then it takes that list and updates each
    of the indexes based on the lower and upper bounds of the
    provided [digitIndex].
    
    Each [digitIndex](0-4) has a [lowerBound] & [upperBound]. They
    are defined in the local Map [_digitIndexBounds].

---
+++ clock_of_clocks/lib/helpers/clock_digit_arranger.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/widgets.dart';
+
+import '../models/analog_clock_model.dart';
+import '../state/clock_digit_arrangements.dart';
+import '../state/clock_state.dart' show amountOfDigits;
+
+/// Represents the amount of clocks in a digit section.
+/// TODO: show image url representing this (graphic representation).
+const int amountOfClocksInDigit = 24;
+
+/// Represents the bounds of a digit with a certain index.
+class _DigitIndexBound {
+  final int lowerBound;
+  final int upperBound;
+
+  const _DigitIndexBound(this.lowerBound, this.upperBound);
+}
+
+/// A [Map] of all digits [DigitIndexBound] and their bounds.
+///
+/// The amount of digits should be equivalent to [amountOfDigits].
+/// The difference between the [upperBound] and [lowerBound]
+/// should always be equivalent to the [amountOfClocksInDigit].
+Map<int, _DigitIndexBound> _digitIndexBounds = {
+  0: _DigitIndexBound(8, 31),
+  1: _DigitIndexBound(32, 55),
+  2: _DigitIndexBound(64, 87),
+  3: _DigitIndexBound(88, 111),
+};
+
+/// Arranges a List<AnalogClockModel> based on the [digitIndex] and [digit].
+List<AnalogClockModel> arrangeClockDigit({
+  @required int digitIndex,
+  @required int digit,
+}) {
+  assert(digitIndex >= 0 && digitIndex <= amountOfDigits);
+  assert(digit >= 0 && digit <= 9);
+
+  var digitIndexBounds = _digitIndexBounds[digitIndex];
+
+  // Assigns a list of [ClockModel]'s arranged based on the provided [digit].
+  List<AnalogClockModel> digitClockArrangements = clockDigitArrangements[digit];
+  assert(digitClockArrangements.length == amountOfClocksInDigit);
+
+  return _updateIndexes(
+    lowerBound: digitIndexBounds.lowerBound,
+    upperBound: digitIndexBounds.upperBound,
+    clockModels: digitClockArrangements,
+  );
+}
+
+/// Update Default Indexes [0..23] with corresponding [lowerBound..upperBound].
+List<AnalogClockModel> _updateIndexes({
+  @required int lowerBound,
+  @required int upperBound,
+  @required List<AnalogClockModel> clockModels,
+}) {
+  assert(lowerBound != null);
+  assert(upperBound != null);
+  assert(clockModels != null);
+
+  for (var i = 0; i < amountOfClocksInDigit; i++) {
+    assert(clockModels[i] != null);
+    clockModels[i].id = lowerBound;
+    assert(lowerBound <= upperBound);
+    lowerBound++;
+  }
+
+  return clockModels;
+}
