434a4a95b18018d1dae0ee260ae739ff77b61f95
Egor Belibov <egor@belibov.dev>
2020-01-14 17:36:19 +0000

    (DEV-ONLY) Add Sonic mode for (faster time updates)

--- clock_of_clocks/lib/state/clock_state.dart
+++ clock_of_clocks/lib/state/clock_state.dart
@@ -1,3 +1,4 @@
+
 // Copyright 2019 Egor Belibov. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -215,5 +216,23 @@ class ClockState extends PropertyChangeNotifier<String> {
 
       if (notifyChanges) notifyListeners(clockModel.id.toString());
     });
+  }
+
+  // -----------------------
+
+  /// **IMPORTANT**: DEV-ONLY
+  ///
+  /// **IMPORTANT**: Don't you dare publish this! (Unless you know what you're doing 😀).
+  void activateSonicMode() {
+    _currentTime = (_currentTime == null)
+        ? DateTime.utc(2020, 01, 01, 15, 00, 00)
+        : _currentTime.add(Duration(seconds: 1));
+
+    _timer = Timer(
+      Duration(milliseconds: 50),
+      _updateTime,
+    );
+
+    _updateClockTimeState();
   }
 }
